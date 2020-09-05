af3172e8b6aa448eb74eebdcf98e17d425843af9
Gimi8 <wojtek.gmiterek@o2.pl>
2020-09-05 11:56:25 +0200

    Dodawnaie i wyswietlanie Albumu

--- src/main/java/io/mbab/sda/groupproject2/repository/CrudRepository.java
+++ src/main/java/io/mbab/sda/groupproject2/repository/CrudRepository.java
@@ -2,8 +2,6 @@ package io.mbab.sda.groupproject2.repository;
 
 import io.mbab.sda.groupproject2.entity.Album;
 
-import io.mbab.sda.groupproject.entity.City;
-
 import java.util.List;
 
 public interface CrudRepository<T, ID> {
@@ -13,8 +11,6 @@ public interface CrudRepository<T, ID> {
   T findById(ID id);
 
   T create(T entity);
-
-
 
   T update(T entity);
 
